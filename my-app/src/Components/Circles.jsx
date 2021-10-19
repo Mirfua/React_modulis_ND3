f501635b5d896a56f7fdafd3d4169acf6e29ff96
Jevgenijus <j.suchodolskis@gmail.com>
2021-10-19 10:48:07 +0300

    fixes

--- my-app/src/Components/Circles.jsx
+++ my-app/src/Components/Circles.jsx
@@ -2,36 +2,34 @@ import { useState } from "react";
 
 function Circles() {
 
-    const [counter1, setCounter1] = useState(0);
-    const [counter2, setCounter2] = useState(0);
+    const [number1, setNumber1] = useState(0);
+    const [number2, setNumber2] = useState(0);
 
     const addNumber = () => {
-        const newCounter = counter1 + 1;
-        setCounter1(newCounter);
+        setNumber1(number1 + 1);
     }
     const addNumber2 = () => {
-        const newCounter2 = counter1 + counter2;
-        setCounter2(newCounter2)
+        setNumber2(number2 + 1);
     }
 
     const reset = () => {
-        setCounter1(0);
-        setCounter2(0);
+        setNumber1(0);
+        setNumber2(0);
     }
 
     return (
         <>
             <div>
                 <div className="circle">
-                    <p>First {counter1}</p>
+                    <span>{number1}</span>   
+                    <button onClick={addNumber}>Add</button>
                 </div>
                 <div className="circle">
-                    <p>Second {counter2}</p>
+                    <span>{number2}</span>
+                    <button onClick={addNumber2}>Add</button>
                 </div>
             </div>
-            <button onClick={addNumber}>Add number in first circle</button>
-            <button onClick={addNumber2}>Add number in second circle</button>
-            <button onClick={reset}>Reset</button>
+            <button onClick={reset}>Reset numbers</button>
         </>
     )
 }
